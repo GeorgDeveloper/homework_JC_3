dae7f953408a042a5e84de9286980f9bc1b49e5a
GeorgDeveloper <ifreyd@yandex.ru>
2023-06-22 19:08:53 +0300

    final

--- src/Employee.java
+++ src/Employee.java
@@ -1,92 +1,117 @@
-public class Employee {
-   private String firstName;
-   private String surName;
-   private String lastName;
-   private int age;
-   private String post;
-   private int salary;
-   private String phoneNumber;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-   public Employee(String firstName, String surName, String lastName, int age, String post, int salary, String phoneNumber) {
-      this.firstName = firstName;
-      this.surName = surName;
-      this.lastName = lastName;
-      this.age = age;
-      this.post = post;
-      this.salary = salary;
-      this.phoneNumber = phoneNumber;
-   }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+public class Employee implements Comparable<Employee> {
+    private String firstName;
+    private String surName;
+    private String lastName;
+    private int age;
+    private String post;
+    private int salary;
+    private String phoneNumber;
 
-   public void setSurName(String surName) {
-      this.surName = surName;
-   }
+    public Employee(String firstName, String surName, String lastName, int age, String post, int salary, String phoneNumber) {
+        this.firstName = firstName;
+        this.surName = surName;
+        this.lastName = lastName;
+        this.age = age;
+        this.post = post;
+        this.salary = salary;
+        this.phoneNumber = phoneNumber;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-   public void setAge(int age) {
-      this.age = age;
-   }
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 
-   public void setPost(String post) {
-      this.post = post;
-   }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-   public void setSalary(int salary) {
-      this.salary = salary;
-   }
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-   public void setPhoneNumber(String phoneNumber) {
-      this.phoneNumber = phoneNumber;
-   }
+    public void setPost(String post) {
+        this.post = post;
+    }
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 
-   public String getSurName() {
-      return surName;
-   }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public String getFirstName() {
+        return firstName;
+    }
 
-   public int getAge() {
-      return age;
-   }
+    public String getSurName() {
+        return surName;
+    }
 
-   public String getPost() {
-      return post;
-   }
+    public String getLastName() {
+        return lastName;
+    }
 
-   public int getSalary() {
-      return salary;
-   }
+    public int getAge() {
+        return age;
+    }
 
-   public String getPhoneNumber() {
-      return phoneNumber;
-   }
+    public String getPost() {
+        return post;
+    }
 
-   public void addSalary (int addsalary){
-      this.salary = this.salary + addsalary;
-   }
+    public int getSalary() {
+        return salary;
+    }
 
-   @Override
-   public String toString() {
-      return "Employee{" +
-              "firstName='" + firstName + '\'' +
-              ", surName='" + surName + '\'' +
-              ", lastName='" + lastName + '\'' +
-              ", age=" + age +
-              ", post='" + post + '\'' +
-              ", salary=" + salary +
-              ", phoneNumber='" + phoneNumber + '\'' +
-              '}';
-   }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void salaryUp(int addsalary, Employee employee, int age) {
+        if (employee.getAge() < age) {
+            this.salary = this.salary + addsalary;
+        }
+    }
+
+    public static void printListEmployees(ArrayList arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i).toString());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Ф.И.О = %s %s %s, Возраст = %d, Должность = %s, Зарплата = %d, Номер телефона = %s",
+                lastName, firstName, surName, age, post, salary, phoneNumber);
+//        return "Ф.И.О. = '" lastName + " " + firstName + '\'' +
+//                ", Отчество = '" + surName + '\'' +
+//                ", Фамилия ='" + lastName + '\'' +
+//                ", age=" + age +
+//                ", post='" + post + '\'' +
+//                ", salary=" + salary +
+//                ", phoneNumber='" + phoneNumber + '\'';
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        if (this.salary == o.salary) {
+            return 0;
+        } else if (this.salary < o.salary) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,31 +7,44 @@ public class Main {
         ArrayList<Employee> arrEmployee = new ArrayList<>();
 
         Employee employee = new Employee("Oleg", "Alekseevich",
-                "Petrov", 32, "mechanikal" , 30000,"89809805466");
+                "Petrov", 50, "mechanikal", 29000, "89809805466");
 
         Employee employee1 = new Employee("Alex", "Alekseevich",
-                "Loto", 36, "mechanikal" , 30000,"89809866467");
+                "Loto", 36, "mechanikal", 30000, "89809866467");
 
         Employee employee2 = new Employee("Max", "Alekseevich",
-                "Ric", 45, "mechanikal" , 32000,"89807705467");
+                "Ric", 45, "mechanikal", 32000, "89807705467");
+
+        Administrator director = new Administrator("Max", "Maksomovich",
+                "Maksimov", 30, "director", 130000, "89807705499");
 
         arrEmployee.add(employee);
         arrEmployee.add(employee1);
         arrEmployee.add(employee2);
+        arrEmployee.add(director);
 
+        //Список сотрудников
+        System.out.println("Список сотрудников");
+        Employee.printListEmployees(arrEmployee);
+
+
+        // Индексация зарплат
+        System.out.println("\nСписок сотрудников с индексированной зарплатой на 5000 в возрасте до 46");
         for (int i = 0; i < arrEmployee.size(); i++) {
-            System.out.println(arrEmployee.get(i).toString());
+            arrEmployee.get(i).salaryUp(5000, arrEmployee.get(i), 46);
         }
 
-        for (int i = 0; i < arrEmployee.size(); i++) {
-            if (arrEmployee.get(i).getAge() < 40){
-                arrEmployee.get(i).addSalary(5000);
-            }
-        }
+        // Список сотрудников с новой зарплатой
+        Employee.printListEmployees(arrEmployee);
 
-        for (int i = 0; i < arrEmployee.size(); i++) {
-            System.out.println(arrEmployee.get(i).toString());
-        }
+        // Отсортированый список сотрудников по зарплате
+        System.out.println("\nОтсортированый список сотрудников по зарплате");
+        arrEmployee.sort(Employee::compareTo);
+        Employee.printListEmployees(arrEmployee);
 
+        // Отсортированый список сотрудников по возрасту
+        System.out.println("\nОтсортированый список сотрудников по возрасту");
+        Collections.sort(arrEmployee, new MyComparator());
+        Employee.printListEmployees(arrEmployee);
     }
 }
